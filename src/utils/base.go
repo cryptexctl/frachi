80dd9d5e9c79fe2a6f201e5ed2b00f15a8631459
Lain Iwakura <lain@iwakurahome.ru>
2025-06-09 12:27:31 +0300

    ok

--- src/utils/base.go
+++ src/utils/base.go
@@ -9,6 +9,8 @@ import (
 func InstallBase() {
 	fmt.Println("Installing base system...")
 	cmd := exec.Command("pacstrap", "/mnt", "base", "linux", "linux-firmware")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Println("pacstrap failed:", err)
 		os.Exit(1)
